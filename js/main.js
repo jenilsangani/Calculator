858e1f388d8d94ce02a79943edb65dab537b6707
JenilSangani <jenilsangani2000@gmail.com>
2022-08-31 15:28:52 +0530

    deploy site

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenvalue = "";
+
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("Button text is", buttonText);
+
+    if (buttonText == "X") {
+      buttonText = "*";
+      screenvalue += buttonText;
+      screen.value = screenvalue;
+
+    } else if (buttonText == "C") {
+      screenvalue = "";
+      screen.value = screenvalue;
+
+    } else if (buttonText == "=") {
+      screen.value = eval(screenvalue);
+
+    } else {
+      screenvalue += buttonText;
+      screen.value = screenvalue;
+      
+    }
+  });
+}
